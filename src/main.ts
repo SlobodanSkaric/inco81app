444f9ea620862f8d2145d66dc7639a36de08ed40
SlobodanSkaric <softverss@gmail.com>
2025-12-07 18:09:30 +0100

    Enable corse for specifed domen

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,14 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true
-  }))
+  }));
+
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
