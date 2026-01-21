72215cac8e853a03587ef5377b1b2a8239e4033c
SlobodanSkaric <softverss@gmail.com>
2026-01-21 10:29:10 +0100

    Testing Adminstrator Services

--- src/modules/administrator/administrator.service.spec.ts
+++ src/modules/administrator/administrator.service.spec.ts
@@ -4,6 +4,7 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Administrator } from 'entitets/entities/Administrator';
 import { Repository } from 'typeorm';
 import { TimeOfWorke } from 'entitets/entities/TimeOfWorke';
+import { ApiResponse } from 'src/misc/api.response.dto';
 
 const mockAdministratorsRepositiort = {
   find: jest.fn(),
@@ -44,6 +45,52 @@ describe('AdministratorService', () => {
       expect(service).toBeDefined();
     });
 
+    it("Get  all administrators", async () =>{
+      const mockAdministrators = [
+        {
+          adminId: 1,
+          name: "Slobodan1",
+          lastname: "Skaric1",
+          email: "slobodan.skaric1@gmail.com",
+          phonenumber:"06054544756",
+        },{
+          adminId: 2,
+          name: "Slobodan2",
+          lastname: "Skaric2",
+          email: "slobodan.skaric2@gmail.com",
+          phonenumber:"060545447567",
+        },
+        {
+          adminId: 3,
+          name: "Slobodan3",
+          lastname: "Skaric3",
+          email: "slobodan.skaric3@gmail.com",
+          phonenumber:"0605454475677",
+        },
+      ];
+
+      mockAdministratorsRepositiort.find.mockResolvedValue(mockAdministrators);
+
+      const result = await service.getAllAdmin();
+      expect(result).toEqual(mockAdministrators);
+      expect(repository.find).toHaveBeenCalled();
+    });
+
+    it("Get administrator by email", async () => {
+      const mockAdministator = {
+        adminId: 1,
+        name: "Slobodan",
+        lastname: "Skaric",
+        email: "slobodan.skaric@gmail.com"
+      }
+
+      mockAdministratorsRepositiort.findOne.mockResolvedValue(mockAdministator);
+
+      const resulte = await service.getByEmail("slobodan.skaric@gmail.com");
+      expect(resulte).toEqual(mockAdministator);
+      expect(repository.findOne).toHaveBeenCalledWith({ where : { email: mockAdministator.email} });
+    });
+
     it("Return administrator when ID is valid", async () => {
       const mocAdministrator = { adminId: 1, name: "Slobodan" };
 
@@ -53,4 +100,56 @@ describe('AdministratorService', () => {
       expect(resulte).toEqual(mocAdministrator);
       expect(repository.findOne).toHaveBeenCalledWith({ where: { adminId: 1 } });
     });
+
+    it("Add administrator", async () => {
+      const mocklAdministrator = {
+        adminId: 1,
+        name: "Slobodan", 
+        lastname: "Skaric",
+        email: "slobodan.skaric@gmail,com",
+        phonenumber: "06054544756",
+        password: "Password1!"
+      }
+
+      const mockAdministratorGetById = {
+        adminId: 1,
+        name: "Slobodan", 
+        lastname: "Skaric",
+        email: "slobodan.skaric@gmail.com",
+        phonenumber: "06054544756",
+      }
+
+      mockAdministratorsRepositiort.save.mockResolvedValue( mocklAdministrator );
+      mockAdministratorsRepositiort.findOne.mockResolvedValue( mockAdministratorGetById );
+
+      const resulte = await service.addAdministratorServices(mocklAdministrator);
+
+      expect(resulte).toEqual(mockAdministratorGetById);
+      expect(repository.save).toHaveBeenCalledWith( expect.objectContaining({
+        name: mocklAdministrator.name,
+        lastname: mocklAdministrator.lastname,
+        email: mocklAdministrator.email,
+        phonenumber: mocklAdministrator.phonenumber,
+        password: expect.any(String),
+      }) );
+    });
+
+    it("Should return ApiResponse error if administrator does not exist" , async () => {
+      mockAdministratorsRepositiort.findOne.mockResolvedValue(null);
+
+      const resulte = await service.getById(999);
+
+      expect(resulte).toBeInstanceOf(ApiResponse);
+      expect(repository.findOne).toHaveBeenCalledWith({ where: { adminId: 999 } });
+    });
+
+    it("Should return null if administrator email does not exist", async () => {
+      mockAdministratorsRepositiort.findOne.mockResolvedValue(null);
+
+      const result = await service.getByEmail("slobodan@gmail.com");
+      expect(result).toBeNull();
+      expect(repository.findOne).toHaveBeenCalledWith({ where : { email: "slobodan@gmail.com"} });
+    });
+
+    //Imlement test for time of worke entity
 });
--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,18 +1,99 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { Repository } from 'typeorm';
+import { Users } from 'entitets/entities/Users';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { mock } from 'node:test';
+import { Customers } from 'entitets/entities/Customers';
+import { Administrator } from 'entitets/entities/Administrator';
 
 describe('UserService', () => {
   let service: UserService;
+  let repository: Repository<Users>;
+
+  const mockUserRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn()
+  }
+
+  const mockCustomerRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn()
+  }
+
+  const mockAdministrorRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn()
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(Users),
+          useValue: mockUserRepository,
+        },
+        {
+          provide: getRepositoryToken(Customers),
+          useValue: mockCustomerRepository,
+        },
+        {
+          provide: getRepositoryToken(Administrator),
+          useValue: mockAdministrorRepository,
+        }
+      ],    
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repository = module.get<Repository<Users>>(getRepositoryToken(Users));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  /*it('should create user', async () => {
+    const dto = {
+      name: "Slobodan",
+      lastname: "Skaric",
+      email: "slobodan.skaric@gmail.com",
+      phonenumber: "0605444756",
+      sumTimeOfWork: "",
+      timeOfWorks: [],
+    }
+
+    const saveUserData  = {
+      userId: 1,
+      ...dto,      
+    }
+
+    mockUserRepository.create.mockReturnValue(saveUserData);
+    mockUserRepository.save.mockResolvedValue(saveUserData);
+
+    const result = await service.addUser(dto as any);
+
+    expect(mockUserRepository.create).toHaveBeenCalledWith(dto);
+    expect(mockUserRepository.save).toHaveBeenCalledWith(saveUserData);
+    expect(result).toEqual(saveUserData);
+  });*/
+ 
+  it('should return user by id', async () => {
+    const userId = 1;
+    const mockUser = {
+      userId: 1,
+      name: "Slobodan",
+      lastname: "Skaric",
+      email: "slobodan.skaric@gmail.com",
+      phonenumber: "0605444756",
+      sumTimeOfWork: "",
+      timeOfWorks: [],
+    }
+
+    mockUserRepository.findOne.mockResolvedValue(mockUser);
+
+    const result = await service.getUserById(userId);
+
+    expect(mockUserRepository.findOne).toHaveBeenCalled();
+    expect(result).toEqual(mockUser);
   });
 });
