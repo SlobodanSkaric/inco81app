1a0cc767d8b5a1773584c7c0a75e9b7abc304ee6
SlobodanSkaric <softverss@gmail.com>
2026-03-20 11:15:57 +0100

    Small change on master

--- src/modules/items/items.service.ts
+++ src/modules/items/items.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Items } from 'entitets/entities/Items';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    constructor(@InjectRepository(Items) private readonly itemsRepository: Repository<Items>) {}
+
+    async findAllItems(): Promise<Items[] | null> { return null}
+}
