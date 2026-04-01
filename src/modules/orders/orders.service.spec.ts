76a338500fa48e2b9f74f3f20460da85c0b94f42
SlobodanSkaric <softverss@gmail.com>
2026-04-01 11:29:41 +0200

    Starts order test basics

--- src/modules/orders/orders.service.spec.ts
+++ src/modules/orders/orders.service.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { Orders } from 'entitets/entities/Orders';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockOrdersRepository = {
+  find: jest.fn(),
+  findOne: jest.fn(),
+  save: jest.fn(),
+}
 
 describe('OrdersService', () => {
   let service: OrdersService;
+  let repository: Repository<Orders>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [OrdersService,
+        {
+          provide: getRepositoryToken(Orders),
+          useValue: mockOrdersRepository,
+        }
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
+    repository = module.get<Repository<Orders>>(getRepositoryToken(Orders));
   });
 
   it('should be defined', () => {
